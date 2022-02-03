1e259967c027510041472eeb12cb0dd4ce67819d
Paula Sgarlatta <paula.sgarlatta@gmail.com>
2022-02-03 18:19:48 +1100

    Create 00_0d_to add thermal aff_PS.R

---
+++ scripts/00_0d_to add thermal aff_PS.R
@@ -0,0 +1,30 @@
+
+#######Not to be in final GH
+
+rm(list=ls()) # cleaning memory
+
+
+# libraries
+library(tidyverse)
+library(here)
+
+spatial_thermal <- read.csv(here::here("from_paula", "SpatialUVC_species_traits_only_thermal.csv") )
+kelp_thermal <- read.csv(here::here("from_paula", "TemporalBRUV_species_traits_only_thermal_kelp.csv") )
+nokelp_thermal <- read.csv(here::here("from_paula", "TemporalBRUV_species_traits_only_thermal_no_kelp.csv") )
+
+thermal_all <- bind_rows(spatial_thermal, kelp_thermal,
+                         nokelp_thermal)%>%
+  distinct(Species, .keep_all = TRUE ) %>%
+  as.data.frame()
+
+#Calculate thermal midpoint
+
+thermal_all <- thermal_all %>% 
+  mutate(thermal=rowMeans(thermal_all[ , c(2,3)], na.rm=TRUE)) %>% 
+  select(Species, thermal)
+
+# saving as csv file
+
+write.csv(thermal_all, file=here::here("data", "raw_data", "thermal_all.csv"), 
+          row.names = FALSE )
+
